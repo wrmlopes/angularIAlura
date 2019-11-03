0f32a4acbe2db33aa004113cf590ef07866bc380
F9901059 Wilson Ricardo Martins Lopes <wrmlopes@bb.com.br>
2019-11-03 20:17:12 -0300

    componente inicial e diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { PhotosModule } from './photos/photos.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
